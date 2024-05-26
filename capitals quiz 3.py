457836e2236f1469b2c10e941e3a70da8fa21a62
Rafshan Shah <rafshan.shah@DETNSW>
2024-05-26 14:19:46 +1000

    Making the GUI
    Started to make the GUI

--- capitals quiz 3.py
+++ capitals quiz 3.py
@@ -1,4 +1,13 @@
-import tkinter as tk
+from tkinter import *
 
-root.tk
-print.
+root = Tk()
+root.geometry("800x800")
+root.title("Asian Country Capitals Quiz")
+
+headingLabel = Label(root, text="Capitals Quiz" , font=("Calibri", 30))
+headingLabel.grid(row=0, column=0)
+
+
+
+
+root.mainloop()
