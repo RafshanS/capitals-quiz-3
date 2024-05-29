79d5ed77662bf88a47dee167c6df06feb8f2d7f3
Rafshan Shah <rafshan.shah@DETNSW>
2024-05-29 19:00:40 +1000

    Further update
    added colour to the buttons, made the quiz window more aesthetic and defined the quit function.

--- capitals quiz 3.py
+++ capitals quiz 3.py
@@ -4,31 +4,51 @@ from tkinter import messagebox
 questions = ["What is the capital of Bangladesh?", "what is the capital of Sri Lanka?", "What is the capital of Tajikistan","What is the capital of Pakistan", "what is the capital of Malaysia"]
 options = [["Dhaka", "Delhi", "Chittagong","Mumbai"], ["Sri Jayawardenepura Kotte", "Mymensingh", "Kandy","Kochi"], ["Ulaanbataar", "Dushanbe","Tripoli","Tashkent"],["Allahabad", "Hyderabad", "Islamabad", "Rangpur"],"Kuala Lumpur", "Jakarta","Bangkok","Kathmandu"]
 answers = ["Dhaka","Sri Jayawardenepura Kotte","Dushanbe","Islamabad","Kuala Lumpur"]
-Button_text = ("Arial", 40, "bold")
-subtitle_text = ("Arial", 20)
+Button_text = ("Arial", 20, "bold")
+question_font = ("Arial", 20)
 Title_text = ("Arial", 50)
 
 root = tk.Tk()
 root.geometry("800x800")
 root.title("Asian Country Capitals Quiz")
+root.configure(bg="#856ff8")
+
+score = 0 
+
+title_label=tk.Label(root, text="Welcome to the Asian Country Capitals Quiz")
+title_label.pack(pady=20)
 
 def Learn():
     messagebox.showinfo("All the countries of Asia and their capitals")
-    print("")
+   
 
-score = 0
     
 def quiz(question_num=0):
-    def check_answer(answer_user, answer):
+    def next_question(answer_user, answer):
+        nonlocal question_num
         global score
-        if user_answer == answer:
+        if answer_user == answer:
             score = score + 1
+        question_num += 1
         if question_num < len(questions) - 1:
+            quiz_window.destroy()
             quiz(question_num + 1)
         else:
-            messagebox.showinfo("Score:", "Your score is:", {score}/{len(questions)})
-            root.deiconify()
-            
+            quiz_window.destroy()
+            check_score()
+    
+    root.withdraw()
+    quiz_window = tk.Toplevel(root)
+    quiz_window.geometry("800x800")
+    quiz_window.configure(bg = "#856ff8")
+    question_label = tk.Label(quiz_window, text=questions[question_num], font=question_font, bg="#56ff8", fg="white")
+    question_label.pack(pady=20)
+    
+    for i in range(4):
+        btn = tk.Radiobutton(quiz_window, text=options[question_num][i], variable=v, value=options[question_num][i], font = Title_text, bg='#4a7abc', fg='yellow', activebackground='green', activeforeground='white')
+        btn.pack()
+    next_button = tk.Button(quiz_window, text="Next", command=lambda: next_question(v.get(), answers[question_num]))
+    next_button.pack()            
 
     
             
